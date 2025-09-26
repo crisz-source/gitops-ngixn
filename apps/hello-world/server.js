565a0a86ca10801b9e5b1ea2cab8634be4de97b7
crisz-source <crishubgit@hotmail.com>
2025-09-26 14:39:56 -0300

    cicd

--- apps/hello-world/server.js
+++ apps/hello-world/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('TESTANDO ARGO 🚀🚀🚀🚀');
+  res.send('TESTANDO ARGO 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀');
 });
 
 app.listen(port, () => {
